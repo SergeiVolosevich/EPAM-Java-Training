7ef0f6d0cd472671f4d1d5d9a72f31555d1423cf
SergeiVolosevich <40494191+SergeiVolosevich@users.noreply.github.com>
2019-06-04 16:31:47 +0300

    getting rid of the literal

--- lesson06/task03/src/by/epam/javatraining/volosevich/lesson06/task03/model/logic/Number.java
+++ lesson06/task03/src/by/epam/javatraining/volosevich/lesson06/task03/model/logic/Number.java
@@ -1,9 +1,11 @@
 package by.epam.javatraining.volosevich.lesson06.task03.model.logic;
 
 public class Number {
+    public static final int NUMBER_MAX_VALUE_FACTORIAL = 170;
+
     public static double calcFactorial(int number) {
         double fact = 1E0;
-        if (number >= 0 && number <= 170) {
+        if (number >= 0 && number <= NUMBER_MAX_VALUE_FACTORIAL) {
             for (int i = 1; i <= number; i++) {
                 fact *= i;
             }
