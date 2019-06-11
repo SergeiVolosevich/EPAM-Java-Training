a5893b1a1eaac012821fdf952881dbbe13bf2954
SergeiVolosevich <40494191+SergeiVolosevich@users.noreply.github.com>
2019-06-11 22:56:41 +0300

    add new methods and tests

--- maintask/maintask01/src/by/epam/javatraining/volosevich/maintask01/model/logic/Vector.java
+++ maintask/maintask01/src/by/epam/javatraining/volosevich/maintask01/model/logic/Vector.java
@@ -1,65 +1,146 @@
 package by.epam.javatraining.volosevich.maintask01.model.logic;
 
+import java.util.Random;
+
 public class Vector {
-    public static int indexOfMax(double[] array) {
-        int indexMax = -1;
-        if (array != null && array.length != 0) {
-            indexMax = 0;
-            for (int i = 1; i < array.length; i++) {
-                if (array[indexMax] < array[i]) {
-                    indexMax = i;
+
+    private static final int LEFT_BOUND = -100;
+    private static final int RIGHT_BOUND = 100;
+    private static final int MAX_SIZE = 1_000_000_000;
+    private static final int INCREASING_VALUE = 5;
+    private static final int START_SIZE = 10;
+
+    private static final Random RANDOM = new Random();
+    private double[] a;
+    private int numOfElem;
+
+
+    public Vector() {
+        a = new double[START_SIZE];
+        numOfElem = 0;
+    }
+
+    public Vector(int numberOfElements) {
+        if (numberOfElements > 0 && numberOfElements < MAX_SIZE) {
+            a = new double[numberOfElements];
+            numOfElem = 0;
+            for (int i = 0; i < numberOfElements; i++) {
+                a[i] = RANDOM.nextInt(RIGHT_BOUND - LEFT_BOUND + 1) + LEFT_BOUND;
+                numOfElem++;
+            }
+        } else {
+            a = new double[0];
+            numOfElem = 0;
+        }
+    }
+
+    public boolean isEmpty() {
+        return numOfElem == 0;
+    }
+
+    public int getSize() {
+        return numOfElem;
+    }
+
+    public double valueOf(int index) {
+        double result = Double.NaN;
+        if (index >= 0 && index < numOfElem) {
+            return result = a[index];
+        }
+        return result;
+    }
+
+    public boolean add(double value) {
+        boolean result = false;
+        if (numOfElem == a.length && numOfElem + INCREASING_VALUE <= MAX_SIZE) {
+            double[] b = new double[a.length + INCREASING_VALUE];
+            for (int i = 0; i < a.length; i++) {
+                b[i] = a[i];
+            }
+            a = b;
+            a[numOfElem++] = value;
+            result = true;
+        } else if (numOfElem + INCREASING_VALUE > MAX_SIZE) {
+            return result;
+        } else {
+            a[numOfElem++] = value;
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        String array = "Vector = []";
+        if (numOfElem != 0) {
+            array = "Vector = [";
+            for (int i = 0; i < numOfElem - 1; i++) {
+                array = array + a[i] + ", ";
+            }
+            array = array + a[numOfElem - 1] + "].";
+        }
+        return array;
+    }
+
+    public double maxValue() {
+        double max = Double.NaN;
+        if (!isEmpty()) {
+            max = a[0];
+            for (int i = 1; i < numOfElem; i++) {
+                if (max < a[i]) {
+                    max = a[i];
                 }
             }
         }
-        return indexMax;
+        return max;
     }
 
-    public static int indexOfMin(double[] array) {
-        int indexMin = -1;
-        if (array != null && array.length != 0) {
-            indexMin = 0;
-            for (int i = 1; i < array.length; i++) {
-                if (array[indexMin] > array[i]) {
-                    indexMin = i;
+    public double minValue() {
+        double min = Double.NaN;
+        if (!isEmpty()) {
+            min = a[0];
+            for (int i = 1; i < numOfElem; i++) {
+                if (min > a[i]) {
+                    min = a[i];
                 }
             }
         }
-        return indexMin;
+        return min;
     }
 
-    public static double calcArithmeticAverage(double[] array) {
+    public double calcArithmeticAverage() {
         double arithmeticAverage = Double.NaN;
-        if (array != null && array.length != 0) {
+        if (!isEmpty()) {
             double sum = 0.0;
-            for (double v : array) {
-                sum += v;
+            for (int i = 0; i < numOfElem; i++) {
+                sum += a[i];
             }
-            arithmeticAverage = sum / array.length;
+            arithmeticAverage = sum / numOfElem;
         }
         return arithmeticAverage;
     }
 
-    public static double calcGeometricAverage(double[] array) {
+    public double calcGeometricAverage() {
         double geometricAverage = -1;
-        if (array != null && array.length != 0) {
+        if (!isEmpty()) {
             double mult = 1.0;
-            for (double v : array) {
-                if (v > 0) {
-                    mult *= v;
+            for (int i = 0; i < numOfElem; i++) {
+                if (a[i] >= 0) {
+                    mult *= a[i];
                 } else {
                     return geometricAverage;
                 }
             }
-            geometricAverage = Math.pow(mult, 1.0 / array.length);
+            geometricAverage = Math.pow(mult, 1.0 / numOfElem);
         }
         return geometricAverage;
     }
 
-    public static boolean isIncreasingOrder(double[] array) {
+    public boolean isIncreasingOrder() {
         boolean result = false;
-        if (array != null && array.length != 0) {
-            for (int i = 1; i < array.length; i++) {
-                if (array[i] < array[i - 1]) {
+        if (!isEmpty()) {
+            for (int i = 1; i < numOfElem; i++) {
+                if (a[i] <= a[i - 1]) {
                     return result;
                 }
             }
@@ -68,11 +149,11 @@ public class Vector {
         return result;
     }
 
-    public static boolean isDecreasingOrder(double[] array) {
+    public boolean isDecreasingOrder() {
         boolean result = false;
-        if (array != null && array.length != 0) {
-            for (int i = 1; i < array.length; i++) {
-                if (array[i] > array[i - 1]) {
+        if (!isEmpty()) {
+            for (int i = 1; i < numOfElem; i++) {
+                if (a[i] >= a[i - 1]) {
                     return result;
                 }
             }
@@ -81,68 +162,169 @@ public class Vector {
         return result;
     }
 
-    public static int indexOfLocalMax(double[] array) {
+    public int indexOfLocalMax() {
         int index = -1;
-        if (array != null && array.length != 0) {
-            for (int i = 1; i + 1 < array.length; i++) {
-                if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
-                    return index = i;
-                }
+        for (int i = 1; i + 1 < numOfElem; i++) {
+            if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+                return index = i;
             }
         }
         return index;
     }
 
-    public static int indexOfLocalMin(double[] array) {
+    public int indexOfLocalMin() {
         int index = -1;
-        if (array != null && array.length != 0) {
-            for (int i = 1; i + 1 < array.length; i++) {
-                if (array[i] < array[i - 1] && array[i] < array[i + 1]) {
-                    return index = i;
-                }
+        for (int i = 1; i + 1 < numOfElem; i++) {
+            if (a[i] < a[i - 1] && a[i] < a[i + 1]) {
+                return index = i;
             }
         }
         return index;
     }
 
-    public static void reverseVector(double[] array) {
-        if (array != null && array.length != 0) {
-            int lastIndex = array.length - 1;
-            double temp;
-            for (int i = 0; i != array.length / 2; i++) {
-                temp = array[i];
-                array[i] = array[lastIndex - i];
-                array[lastIndex - i] = temp;
-            }
+    public void reverseVector() {
+        int lastIndex = numOfElem - 1;
+        for (int i = 0; i != numOfElem / 2; i++) {
+            swap(a, i, lastIndex - i);
         }
     }
 
-    public static int linearSearch(double[] array, double elementToSearch) {
-        int index = -1;
-        if (array != null && array.length != 0) {
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] == elementToSearch) {
-                    return index = i;
-                }
+    public int linearSearch(double elementToSearch) {
+        int index;
+        for (int i = 0; i < numOfElem; i++) {
+            if (a[i] == elementToSearch) {
+                return index = i;
             }
         }
-        return index;
+
+        return -1;
     }
 
-    public static int binarySearch(double[] array, double elementToSearch) {
+    public int binarySearch(double elementToSearch) {
         int firstIndex = 0;
-        int lastIndex = array.length - 1;
+        int lastIndex = numOfElem - 1;
 
         while (firstIndex <= lastIndex) {
             int middleIndex = (firstIndex + lastIndex) / 2;
-            if (array[middleIndex] == elementToSearch) {
+            if (a[middleIndex] == elementToSearch) {
                 return middleIndex;
-            } else if (array[middleIndex] < elementToSearch) {
+            } else if (a[middleIndex] < elementToSearch) {
                 firstIndex = middleIndex + 1;
             } else {
                 lastIndex = middleIndex - 1;
             }
         }
         return -1;
+    }
+
+    public void mergeSort() {
+        double[] arrayWork = new double[numOfElem];
+        recMergeSort(a, arrayWork, 0, numOfElem - 1);
+    }
+
+    private static void recMergeSort(double[] array, double[] workSpace, int start, int end) {
+        if (start < end) {
+            int middle = (start + end) / 2;
+            recMergeSort(array, workSpace, start, middle);
+            recMergeSort(array, workSpace, middle + 1, end);
+
+            merge(array, workSpace, start, middle, end);
+        }
+    }
+
+    private static void merge(double[] array, double[] workSpace, int start, int middle, int end) {
+        int i = 0;
+        int str = start;
+        int midBound = middle + 1;
+        int elements = end - start + 1;
+        while (start <= middle && midBound <= end) {
+            if (array[start] < array[midBound]) {
+                workSpace[i++] = array[start++];
+            } else {
+                workSpace[i++] = array[midBound++];
+            }
+        }
+
+        while (start <= middle) {
+            workSpace[i++] = array[start++];
+        }
+        while (midBound <= end) {
+            workSpace[i++] = array[midBound++];
+        }
+
+        for (int j = 0; j < elements; j++) {
+            array[str + j] = workSpace[j];
+        }
+    }
+
+    public void quickSort() {
+        recQuickSort(a, 0, numOfElem - 1);
+    }
+
+    private static void recQuickSort(double[] array, int start, int end) {
+        if (start < end) {
+            double pivot = array[end];
+
+            int partition = partition(array, start, end, pivot);
+
+            recQuickSort(array, start, partition - 1);
+            recQuickSort(array, partition + 1, end);
+        }
+    }
+
+    private static int partition(double[] array, int wall, int end, double pivot) {
+        int left = wall;
+        while (left != end) {
+            if (pivot > array[left]) {
+                swap(array, wall, left);
+                wall++;
+            }
+            left++;
+        }
+        swap(array, wall, end);
+        return wall;
+    }
+
+    private static void swap(double[] array, int i, int j) {
+        double temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public void bubbleSort() {
+        boolean notSorted = true;
+        for (int i = numOfElem - 1; i > 0 && notSorted; i--) {
+            notSorted = false;
+            for (int j = 0; j < i; j++) {
+                if (a[j] > a[j + 1]) {
+                    swap(a, j, j + 1);
+                    notSorted = true;
+                }
+            }
+        }
+    }
+
+    public void selectSort() {
+        for (int i = 0; i < numOfElem - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < numOfElem; j++) {
+                if (a[j] < a[index]) {
+                    index = j;
+                }
+            }
+            swap(a, i, index);
+        }
+    }
+
+    public void insertSort() {
+        for (int i = 1; i < numOfElem; i++) {
+            double temp = a[i];
+            int j = i;
+            while (j > 0 && a[j - 1] >= temp) {
+                a[j] = a[j - 1];
+                j--;
+            }
+            a[j] = temp;
+        }
     }
 }
--- maintask/maintask01/test/by/epam/javatraining/volosevich/maintask01/model/logic/VectorTest.java
+++ maintask/maintask01/test/by/epam/javatraining/volosevich/maintask01/model/logic/VectorTest.java
@@ -1,5 +1,432 @@
+package by.epam.javatraining.volosevich.maintask01.model.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
 
 public class VectorTest {
 
+    private Vector vector;
+    private Vector zeroVector;
+    private Vector manualVector;
+    private double[] arrayToSort;
+    private double[] sortedArrayByClassVector;
+    private double[] sortedArrayByClassArrays;
+
+    @Before
+    public void setUp() throws Exception {
+        vector = new Vector(100);
+        zeroVector = new Vector(0);
+        manualVector = new Vector();
+
+        arrayToSort = new double[vector.getSize()];
+        for (int i = 0; i < arrayToSort.length; i++) {
+            arrayToSort[i] = vector.valueOf(i);
+        }
+
+        sortedArrayByClassVector = new double[vector.getSize()];
+        sortedArrayByClassArrays = new double[vector.getSize()];
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertFalse(vector.isEmpty());
+    }
+
+    @Test
+    public void testValueOfByNegativeValue() {
+        double expected = Double.NaN;
+        assertEquals(expected, vector.valueOf(-1), 0);
+    }
+
+    @Test
+    public void testValueOfByValidValue() {
+        double expected = arrayToSort[1];
+        assertEquals(expected, vector.valueOf(1), 0);
+    }
+
+    @Test
+    public void testValueOfByInvalidValue() {
+        double expected = Double.NaN;
+        assertEquals(expected, vector.valueOf(100), 0);
+    }
+
+    @Test
+    public void testMaxValue() {
+        double expected = vector.maxValue();
+        Arrays.sort(arrayToSort);
+
+        assertEquals(expected, arrayToSort[arrayToSort.length - 1], 0);
+    }
+
+    @Test
+    public void testMaxValueByZeroVector() {
+        double result = Double.NaN;
+        double expected = zeroVector.maxValue();
+
+        assertEquals(expected, result, 0);
+    }
+
+    @Test
+    public void testMinValue() {
+        double expected = vector.minValue();
+        Arrays.sort(arrayToSort);
+
+        assertEquals(expected, arrayToSort[0], 0);
+    }
+
+    @Test
+    public void testMinValueByZeroVector() {
+        double expected = zeroVector.minValue();
+        double result = Double.NaN;
+
+        assertEquals(expected, result, 0);
+    }
+
+    @Test
+    public void testCalcArithmeticAverage() {
+        manualVector.add(1);
+        manualVector.add(12);
+        manualVector.add(3);
+        manualVector.add(8);
+        manualVector.add(9);
+        manualVector.add(9);
+
+        double expected = 7.0;
+
+        assertEquals(expected, manualVector.calcArithmeticAverage(), 0);
+    }
+
+    @Test
+    public void testCalcArithmeticAverageByZeroVector() {
+        double expected = Double.NaN;
+
+        assertEquals(expected, zeroVector.calcArithmeticAverage(), 0);
+    }
+
+    @Test
+    public void testCalcGeometricAverage() {
+        manualVector.add(1);
+        manualVector.add(12);
+        manualVector.add(3);
+        manualVector.add(8);
+        manualVector.add(9);
+        manualVector.add(9);
+
+        double expected = 5.345392;
+
+        assertEquals(expected, manualVector.calcGeometricAverage(), 0.001);
+    }
+
+    @Test
+    public void testCalcGeometricAverageByZeroVector() {
+        double expected = -1;
+
+        assertEquals(expected, zeroVector.calcGeometricAverage(), 0.001);
+    }
+
+    @Test
+    public void testIsIncreasingOrderTrue() {
+        manualVector.add(1);
+        manualVector.add(2);
+        manualVector.add(3);
+        manualVector.add(4);
+        manualVector.add(5);
+        manualVector.add(6);
+        manualVector.add(7);
+
+        assertTrue(manualVector.isIncreasingOrder());
+    }
+
+    @Test
+    public void testIsIncreasingOrderFalse() {
+        manualVector.add(1);
+        manualVector.add(7);
+        manualVector.add(3);
+        manualVector.add(4);
+        manualVector.add(5);
+        manualVector.add(6);
+        manualVector.add(7);
+
+        assertFalse(manualVector.isIncreasingOrder());
+    }
+
+    @Test
+    public void testIsIncreasingOrderByDuplicateValue() {
+        manualVector.add(1);
+        manualVector.add(2);
+        manualVector.add(3);
+        manualVector.add(4);
+        manualVector.add(5);
+        manualVector.add(5);
+        manualVector.add(7);
+
+        assertFalse(manualVector.isIncreasingOrder());
+    }
+
+    @Test
+    public void testIsIncreasingOrderByZeroVector() {
+
+        assertFalse(zeroVector.isIncreasingOrder());
+    }
+
+    @Test
+    public void testIsDecreasingOrderTrue() {
+        manualVector.add(7);
+        manualVector.add(6);
+        manualVector.add(5);
+        manualVector.add(4);
+        manualVector.add(3);
+        manualVector.add(2);
+        manualVector.add(1);
+        manualVector.add(0);
+
+        assertTrue(manualVector.isDecreasingOrder());
+    }
+
+    @Test
+    public void testIsDecreasingOrderFalse() {
+        manualVector.add(7);
+        manualVector.add(6);
+        manualVector.add(5);
+        manualVector.add(12);
+        manualVector.add(3);
+        manualVector.add(2);
+        manualVector.add(1);
+        manualVector.add(0);
+
+        assertFalse(manualVector.isDecreasingOrder());
+    }
+
+    @Test
+    public void testIsDecreasingOrderByDuplicateValue() {
+        manualVector.add(7);
+        manualVector.add(6);
+        manualVector.add(5);
+        manualVector.add(4);
+        manualVector.add(3);
+        manualVector.add(2);
+        manualVector.add(2);
+        manualVector.add(2);
+
+        assertFalse(manualVector.isDecreasingOrder());
+    }
+
+    @Test
+    public void testIsDecreasingOrderByZeroVector() {
+
+        assertFalse(zeroVector.isDecreasingOrder());
+    }
+
+    @Test
+    public void testIndexOfLocalMaxValidValue() {
+        manualVector.add(1);
+        manualVector.add(16);
+        manualVector.add(3);
+        manualVector.add(15);
+        manualVector.add(1);
+        manualVector.add(-7);
+        manualVector.add(15);
+
+        int expected = 1;
+
+        assertEquals(expected, manualVector.indexOfLocalMax());
+    }
+
+    @Test
+    public void testIndexOfLocalMaxInvalidValue() {
+        manualVector.add(1);
+        manualVector.add(16);
+        manualVector.add(16);
+        manualVector.add(16);
+        manualVector.add(16);
+        manualVector.add(16);
+        manualVector.add(16);
+
+        int expected = -1;
+
+        assertEquals(expected, manualVector.indexOfLocalMax());
+    }
+
+    @Test
+    public void testIndexOfLocalMaxByZeroVector() {
+        int expected = -1;
+
+        assertEquals(expected, zeroVector.indexOfLocalMax());
+    }
+
+    @Test
+    public void testIndexOfLocalMinValidValue() {
+        manualVector.add(1);
+        manualVector.add(-116);
+        manualVector.add(3);
+        manualVector.add(15);
+        manualVector.add(1);
+        manualVector.add(-7);
+        manualVector.add(15);
+
+        int expected = 1;
+
+        assertEquals(expected, manualVector.indexOfLocalMin());
+    }
+
+    @Test
+    public void testIndexOfLocalMinInvalidValue() {
+        manualVector.add(1);
+        manualVector.add(-116);
+        manualVector.add(-116);
+        manualVector.add(-116);
+        manualVector.add(1);
+        manualVector.add(-7);
+        manualVector.add(-15);
+
+        int expected = -1;
+
+        assertEquals(expected, manualVector.indexOfLocalMin());
+    }
+
+    @Test
+    public void testIndexOfLocalMinByZeroVector() {
+        int expected = -1;
+
+        assertEquals(expected, zeroVector.indexOfLocalMin());
+    }
+
+    @Test
+    public void testReverseVector() {
+        for (int i = 0; i < sortedArrayByClassVector.length; i++) {
+            sortedArrayByClassVector[i] = vector.valueOf(i);
+        }
+        vector.reverseVector();
+
+        boolean expected = true;
+        int lastIndex = sortedArrayByClassVector.length - 1;
+        for (int i = 0; i < vector.getSize(); i++) {
+            if (vector.valueOf(i) != sortedArrayByClassVector[lastIndex - i]) {
+                expected = false;
+            }
+        }
+
+        assertTrue(expected);
+    }
+
+    @Test
+    public void testLinearSearch() {
+        manualVector.add(15);
+        manualVector.add(123);
+        manualVector.add(17);
+        manualVector.add(123);
+        manualVector.add(143);
+        manualVector.add(298);
+
+        int result = manualVector.linearSearch(123);
+        int expected = 1;
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testLinearSearchByZeroVector() {
+        int expected = -1;
+
+        assertEquals(expected, zeroVector.linearSearch(12));
+    }
+
+    @Test
+    public void testBinarySearch() {
+        manualVector.add(15);
+        manualVector.add(123);
+        manualVector.add(17);
+        manualVector.add(123);
+        manualVector.add(143);
+        manualVector.add(298);
+
+        manualVector.quickSort();
+
+        int result = manualVector.binarySearch(298);
+        int expected = 5;
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testBinarySearchByZeroVector() {
+        int expected = -1;
+
+        assertEquals(expected, zeroVector.binarySearch(1));
+    }
+
+    @Test
+    public void mergeSort() {
+        vector.mergeSort();
+        for (int i = 0; i < sortedArrayByClassVector.length; i++) {
+            sortedArrayByClassVector[i] = vector.valueOf(i);
+        }
+
+        Arrays.sort(arrayToSort);
+        for (int i = 0; i < sortedArrayByClassArrays.length; i++) {
+            sortedArrayByClassArrays[i] = arrayToSort[i];
+        }
+        assertArrayEquals(sortedArrayByClassVector, sortedArrayByClassArrays, 0);
+    }
+
+    @Test
+    public void testQuickSort() {
+        vector.quickSort();
+        for (int i = 0; i < sortedArrayByClassVector.length; i++) {
+            sortedArrayByClassVector[i] = vector.valueOf(i);
+        }
+
+        Arrays.sort(arrayToSort);
+        for (int i = 0; i < sortedArrayByClassArrays.length; i++) {
+            sortedArrayByClassArrays[i] = arrayToSort[i];
+        }
+        assertArrayEquals(sortedArrayByClassVector, sortedArrayByClassArrays, 0);
+    }
+
+    @Test
+    public void testBubbleSort() {
+        vector.bubbleSort();
+        for (int i = 0; i < sortedArrayByClassVector.length; i++) {
+            sortedArrayByClassVector[i] = vector.valueOf(i);
+        }
+
+        Arrays.sort(arrayToSort);
+        for (int i = 0; i < sortedArrayByClassArrays.length; i++) {
+            sortedArrayByClassArrays[i] = arrayToSort[i];
+        }
+        assertArrayEquals(sortedArrayByClassVector, sortedArrayByClassArrays, 0);
+    }
+
+    @Test
+    public void testSelectSort() {
+        vector.selectSort();
+        for (int i = 0; i < sortedArrayByClassVector.length; i++) {
+            sortedArrayByClassVector[i] = vector.valueOf(i);
+        }
+
+        Arrays.sort(arrayToSort);
+        for (int i = 0; i < sortedArrayByClassArrays.length; i++) {
+            sortedArrayByClassArrays[i] = arrayToSort[i];
+        }
+        assertArrayEquals(sortedArrayByClassVector, sortedArrayByClassArrays, 0);
+    }
+
+    @Test
+    public void testInsertSort() {
+        vector.insertSort();
+        for (int i = 0; i < sortedArrayByClassVector.length; i++) {
+            sortedArrayByClassVector[i] = vector.valueOf(i);
+        }
+
+        Arrays.sort(arrayToSort);
+        for (int i = 0; i < sortedArrayByClassArrays.length; i++) {
+            sortedArrayByClassArrays[i] = arrayToSort[i];
+        }
+        assertArrayEquals(sortedArrayByClassVector, sortedArrayByClassArrays, 0);
+    }
 }
